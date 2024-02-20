b67f3fc89da3e959e2e32a8d5b24211faa6c5985
Rahmat Hidayat <rhmalang@gmail.com>
2024-02-20 12:52:57 +0700

    commit PWL 2024

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PWL 2024</title>
+    <style>
+          body {
+  font-family: orc a extanded, orc a extanded;
+  margin: 0;
+  padding: 0;
+}
+
+.ktp {
+  width: 800px;
+  margin: 20px auto;
+  /* border: 2px solid #000; */
+  border-radius: 5px;
+  padding: 20px;
+  background-image: url('https://i.pinimg.com/originals/51/15/99/51159921c12902effca79e56584605c1.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+table {
+  width: 100%;
+  /* border: solid 2px red; */
+}
+td {
+  /* border: solid 2px blue; */
+  
+}
+
+.header {
+  text-align: center;
+}
+
+.header img {
+  width: 100px;
+  height: 100px;
+}
+
+.label {
+  font-weight: bold;
+}
+
+.footer {
+  text-align: center;
+}
+
+.note {
+  font-style: italic;
+}
+    </style>
+</head>
+<body>
+  <div class="ktp">
+    <table >
+      <tr>
+        <td colspan="3" class="header">
+          <h1>PROVINSI JAWA TIMUR</h1>
+          <h1>KOTA MALANG</h1>
+        </td>
+      </tr>
+      <tr>
+        <td class="label">NIK</td>
+        <td class="value">: &nbsp;1401101307030005</td>
+        <td rowspan="14" style="text-align: center;">
+          <img width="140" height="190" src="https://akademik.polinema.ac.id/upload_dir/foto_ktm_valid/2021/2131710043-vfpam-ghGRR.jpeg" alt=""><br>
+          <span>KOTA MALANG</span><br>
+          <span>11-09-2020</span><br>
+          <img width="100" src="https://e7.pngegg.com/pngimages/332/673/png-clipart-brazil-signature-president-linha-do-tempo-angle-white.png" alt="">
+        </td>
+      </tr>
+      <tr>
+        <td class="label">Nama</td>
+        <td class="value">:RAHMAT HIDAYAT</td>
+      </tr>
+      <tr>
+        <td class="label">Tempat/Tanggal Lahir</td>
+        <td class="value">:KAMPAR, 13-07-2003</td>
+      </tr>
+      <tr>
+        <td class="label">Jenis Kelamin</td>
+        <td class="value">:LAKI-LAKI</td>
+      </tr>
+      <tr>
+        <td class="label">Alamat</td>
+        <td class="value">:Jl. SIMPANG KH YUSUF BLOK A-16</td>
+      </tr>
+      <tr>
+        <td class="label">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RT/RW</td>
+        <td class="value">:001/006</td>
+      </tr>
+      <tr>
+        <td class="label">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kel/Desa</td>
+        <td class="value">:TASIKMADU</td>
+      </tr>
+      <tr>
+        <td class="label">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kecamatan</td>
+        <td class="value">:LOWOKWARU</td>
+      </tr>
+      <tr>
+        <td class="label">Agama</td>
+        <td class="value">:ISLAM</td>
+      </tr>
+      <tr>
+        <td class="label">Status Perkawinan</td>
+        <td class="value">:BELUM KAWIN</td>
+      </tr>
+      <tr>
+        <td class="label">Pekerjaan</td>
+        <td class="value">:PELAJAR/MAHASISWA</td>
+      </tr>
+      <tr>
+        <td class="label">Kewarganegaraan</td>
+        <td class="value">:WNI</td>
+      </tr>
+      <tr>
+        <td class="label">Berlaku Hingga</td>
+        <td class="value">:SEUMUR HIDUP</td>
+      </tr>
+      
+    </table>
+    <br>
+  </div>
+</body>
+</html>
