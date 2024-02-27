9eb9d7260b8b48d5420f269c3920047b376555b4
Rahmat Hidayat <rhmalang@gmail.com>
2024-02-27 12:39:52 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/user/{name}', function ($name=null) {
+    return ('Rahmat Hidayat');
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return ('Rahmat Hidayat').$name;
+});
+
+Route::get('/mahasiswa', function () {
+    $arrMahasiswa = ["Rahmat Hidayat", "bangkit", "perly"];
+
+    return view('POLITEKNIK NEGERI MALANG',['mahasiswa'=> $arrMahasiswa]);
 });
